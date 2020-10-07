f77ad63c359de3f13182933f17d898193000f91f
CarlosEduardoCortesC <carlossistemas91@gmail.com>
2020-10-06 23:43:55 -0500

    Update product.js

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,5 +1,7 @@
 const server = require('express').Router();
 const { Product } = require('../db.js');
+var body_parser = require('body-parser');
+var create=201;
 
 server.get('/', (req, res, next) => {
 	Product.findAll()
@@ -10,3 +12,37 @@ server.get('/', (req, res, next) => {
 });
 
 module.exports = server;
+
+server.post('/',function(req,res){
+	if(!req){
+		res.status(400).send("Datos incompletos");
+	}
+	var skun=req.body.skun;	
+	var name=req.body.name;
+	var description=req.body.description;
+	var price= req.body.price;
+	var dimension=req.body.dimension;
+	var category_id=req.body.category_id;
+	var stock=req.body.stock;
+	var assessment=req.body.assessment;
+
+	Product.findOrCreate({
+		where:req.body.name
+	}).then(function(products){
+		return Product.Create({
+			skun:skun,
+			name:name,
+			description=description,
+			price:price,
+			dimension:dimension,
+			category_id:category_id,
+			stock:stock,
+			assessment:assessment
+		}).then(res.status(create).send("Producto Creado Correctamente."));
+
+	})
+
+
+
+
+});
