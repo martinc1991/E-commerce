bd98345d789c16e3c2536d91e94beb427d50a99c
Camilolin <camilolindarte1992@gmail.com>
2020-10-07 17:03:54 -0500

    Componentes para Crud de Producto

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import PrinciapalAdmin from './components/AdminForm/pageP'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
