99ca3fbc2c94d50f283f6510fac42a383bab2fb0
CarlosEduardoCortesC <carlossistemas91@gmail.com>
2020-10-07 01:00:44 -0500

    createOrFindProduct
    Se añade ruta de eliminar producto y su validacion

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,5 +1,7 @@
 const server = require('express').Router();
 const { Product } = require('../db.js');
+const eliminar = 200;
+const badRequest =400;
 
 server.get('/', (req, res, next) => {
 	Product.findAll()
@@ -8,5 +10,25 @@ server.get('/', (req, res, next) => {
 		})
 		.catch(next);
 });
+
+server.delete('/:id',function(req,res,next){
+	var idProducto = req.params.id;
+
+	Product.destroy({ 
+		where: 
+			{ 
+				id: idProducto //this will be your id that you want to delete 
+			}
+		}).then(function(rowDeleted){ 
+			// rowDeleted will return number of rows deleted 
+			if(rowDeleted === 1){ 
+				console.log('Deleted successfully'); 
+				res.status(eliminar).send('Deleted successfully');
+			}else {
+				res.status(badRequest).send('Deleted successfully');
+			}
+
+		});
+})
 
 module.exports = server;
