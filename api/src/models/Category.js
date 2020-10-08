c19b876d032a0129ca8f4ab5232c866babea6745
Cristovk <63594174+Cristovk@users.noreply.github.com>
2020-10-07 21:42:27 -0300

    s23 search?query={valor} no test 200
    agregada ruta para la consulta solicitada, con test entregando error 400 pero sin aun funcionar el automatizado para crear el 200 y misma correcion en test 200 de la branch 22. cambios en el models/Category para correcto funcionamiento

--- api/src/models/Category.js
+++ api/src/models/Category.js
@@ -1,11 +1,29 @@
-const { Datatypes } = require('sequelize');
-const { conn } = require('../db');
+const { DataTypes, Sequelize } = require("sequelize");
 
-const Category = conn.define('category', {
-    category_name: {
-        type: Datatypes.STRING,
-        allowNull: false,
-    }
-});
-
-module.exports = Category;
+module.exports = (sequelize) => {
+    sequelize.define('categories', {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isAlphanumeric: true
+            }
+        },
+        description: {
+            type: DataTypes.TEXT,
+        }
+    });
+};
+//
+// const { Datatypes } = require('sequelize');
+// const { conn } = require('../db');
+//
+//
+// const Category = conn.define('category', {
+//     category_name: {
+//         type: Datatypes.STRING,
+//         allowNull: false,
+//     }
+// });
+//
+// module.exports = Category;
--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,5 +1,13 @@
 const server = require('express').Router();
 const { Product } = require('../db.js');
+const {Op} = require('sequelize');
+
+// CODIGO VARIABLES DE STATUS
+const OK = 200;
+const CREATE_OK = 201;
+const ERROR = 400;
+const NOT_FOUND = 404;
+const ERROR_SERVER = 500;
 
 server.get('/', (req, res, next) => {
 	Product.findAll()
@@ -8,5 +16,53 @@ server.get('/', (req, res, next) => {
 		})
 		.catch(next);
 });
+
+server.get('/category/:category_id', (req, res) =>{
+	//retornar todos los productos de una categoria
+	const {category_id} = req.params;
+
+	Product.findByFK(category_id,{
+					include: {
+					model: category_pro_id
+					}
+	})// revisar si se debe retornar solo el nombre o todos
+	//los detalles del producto
+	.then((products) => {
+		res.status(OK).send('Los productos guardados serian:', {products});
+	})
+
+});
+
+
+
+server.get('/search', (req, res) =>{
+	const value = req.query.query;
+
+//SELECT name, describe FROM product WHERE name OR describe LIKE '%value%'
+	if(!value || !isNaN(value)){
+		return res.sendStatus(ERROR);
+	}// Encuentra todos los valores de name y describe que coicidan con value
+	Product.findAll({WHERE:{
+		[Op.or]:[{name: value}, {describe: value}],
+		[Op.like]:'%value%'
+	}
+})
+.then((values) => {
+		res.status(OK).send({values})
+		})
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = server;
