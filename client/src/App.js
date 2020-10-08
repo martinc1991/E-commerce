53daf9bc1ecc6e2810048a90f2ee8001108203d6
Camilolin <camilolindarte1992@gmail.com>
2020-10-08 15:09:40 -0500

    Crud Category version 1

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import PrinciapalAdmin from './components/AdminForm/pageP'
+import Product from './components/AdminForm/product'
+import Category from './components/AdminForm/Categorys'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Route, Switch, Link} from 'react-router-dom'
 
 
 function App() {
   return (
-    <PrinciapalAdmin/>
+    <div>
+    {/* <PrinciapalAdmin/> */}
+    
+    <Switch>
+      <Route path='/admin' exact={true} component={PrinciapalAdmin}/>
+      <Route path='/admin/product' exact={true} component={Product}/>
+      <Route path='/admin/category' exact={true} component={Category}/>
+      <Route path='/' exact={true} >
+            <Link to="/admin">Admin</Link>
+      </Route>
+
+    </Switch>
+    </div>
   );
 }
 
--- client/src/components/AdminForm/menu.jsx
+++ client/src/components/AdminForm/menu.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
+import { Nav, Navbar, Form, Button, FormControl } from 'react-bootstrap';
+import { Link } from 'react-router-dom'
+import s from '../styles/styles.module.css'
 
 const Menu = ()=> {
     return (
         <div>
-            <ul>
-                <li><a href="#">Product</a></li>
-                <li><a href="#">Category</a></li>
-            </ul>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="#home">G4-ADMIN</Navbar.Brand>
+                <Nav className="mr-auto" >
+                    
+                        <li className={s.list__nav}><Link to="/admin/product">Product</Link></li>
+                        <li className={s.list__nav}><Link to="/admin/category">Categorys</Link></li>
+                    
+                </Nav>
+                <Form inline>
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <Button variant="outline-info">Search</Button>
+                </Form>
+                <Nav.Link href="#features">Logout</Nav.Link>
+            </Navbar>
         </div>
     )
 }
