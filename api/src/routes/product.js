6df5cea510a09594f5c16ff3ca1006909b66fcdd
CarlosEduardoCortesC <carlossistemas91@gmail.com>
2020-10-08 17:00:55 -0500

    Modificar el producto
    Se crea la ruta para modificar el producto

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,5 +1,10 @@
 const server = require('express').Router();
 const { Product } = require('../db.js');
+const OK = 200;
+const CREATE_OK = 201;
+const ERROR = 400;
+const NOT_FOUND = 404;
+const ERROR_SERVER = 500;
 
 server.get('/', (req, res, next) => {
 	Product.findAll()
@@ -7,6 +12,26 @@ server.get('/', (req, res, next) => {
 			res.send(products);
 		})
 		.catch(next);
+});
+
+server.put('/:id', function (req, res) {
+	const idProduct = req.params.id;
+	Product.update({
+		name: req.body.name,
+		description:req.body.description,
+		price:req.body.price,
+		stock:req.body.stock,
+		dimentions:req.body.dimentions,
+		rating:req.body.rating
+
+	},
+		{
+		where: {
+			id: idProduct,
+		}
+	}).then(function(producto){
+		res.status(OK).send({producto});
+	});
 });
 
 module.exports = server;
