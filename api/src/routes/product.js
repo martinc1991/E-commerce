e932164731f5d0916c974e7099b8fc046d08a1bc
Cristovk <63594174+Cristovk@users.noreply.github.com>
2020-10-09 17:03:09 -0300

    s24 casi listo falta tomarlo con los cambios que ustedes realizaron

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,5 +1,5 @@
 const server = require('express').Router();
-const { INTEGER, STRING } = require('sequelize/types');
+// const { INTEGER, STRING } = require('sequelize');
 const { Product } = require('../db.js');
 const {Op} = require('sequelize');
 
@@ -13,121 +13,127 @@ const ERROR_SERVER = 500;
 server.get('/', (req, res, next) => {
 	Product.findAll()
 		.then(products => {
-			res.status(OK);
-			res.send( products);
+			res.status(OK).send( products);
 		})
 		.catch(next);
 });
 
 
-//s26
-server.put('/:id', function (req, res) {
+	//s26
+	server.put('/:id', function (req, res) {
 
-	const idProduct = req.params.id;
+		const idProduct = req.params.id;
 
-	Product.update({
+		Product.update({
 
-		name: req.body.name,
-		description:req.body.description,
-		price:req.body.price,
-		stock:req.body.stock,
-		dimention:req.body.dimention,
-		rating:INTEGER,
-		thumbnail:STRING
+			name: req.body.name,
+			description:req.body.description,
+			price:req.body.price,
+			stock:req.body.stock,
+			dimention:req.body.dimention,
+			rating:INTEGER,
+			thumbnail:STRING
 
-	},
-		{
-		where: {
-			id: idProduct,
-		}
-	}).then(function(producto){
-		res.status(OK).json({message:"producto Modificado correctamente",producto});
-	}).catch(
-		function(error){
-			console.log("error al modificar producto",error);
-			res.status(ERROR_SERVER).json({message:"error al modificar el producto",error})
-		});
-});
-
-//s25
-server.post('/',function(req,res){
-	if(!req){
-		res.status(ERROR).send("Datos incompletos");
-	}
-	const {name,description,price,stock,dimention,rating,thumbnail}=req.body
-
-		Product.create({
-		name:name,
-		description:description,
-		price:price,
-		stock:stock,
-		dimention:dimention,
-		rating:rating,
-		thumbnail:thumbnail
-
-	}).then((products)=>{
-		return res.status(CREATE_OK).json({message:"Producto Creado Correctamente.",data:products})
-	});
-});
-
-//s27
-server.delete('/:id',function(req,res,next){
-	var idProducto = req.params.id;
-
-	Product.destroy({
-		where:
+		},
 			{
-				id: idProducto //this will be your id that you want to delete
+			where: {
+				id: idProduct,
 			}
-		}).then(function(rowDeleted){
-			// rowDeleted will return number of rows deleted
-			if(rowDeleted >= 1){
-				console.log('Deleted successfully');
-				res.status(OK).json({message:'Deleted successfully',rowDeleted});
-			}else {
-				res.status(ERROR).json({message:'Deleted failed',rowDeleted});
-			}
+		}).then(function(producto){
+			res.status(OK).json({message:"producto Modificado correctamente",producto});
+		}).catch(
+			function(error){
+				console.log("error al modificar producto",error);
+				res.status(ERROR_SERVER).json({message:"error al modificar el producto",error})
+			});
+	});
 
+	//s25
+	server.post('/',function(req,res){
+		if(!req){
+			res.status(ERROR).send("Datos incompletos");
+		}
+		const {name,description,price,stock,dimention,rating,thumbnail}=req.body
+
+			Product.create({
+			name:name,
+			description:description,
+			price:price,
+			stock:stock,
+			dimention:dimention,
+			rating:rating,
+			thumbnail:thumbnail
+
+		}).then((products)=>{
+			return res.status(CREATE_OK).json({message:"Producto Creado Correctamente.",data:products})
 		});
-})
-//s23
-server.get('/search', (req, res) =>{
-	const value = req.query.query;
+	});
 
-//SELECT name, describe FROM product WHERE name OR describe LIKE '%value%'
-	if(!value || !isNaN(value)){
-		return res.sendStatus(ERROR);
-	}// Encuentra todos los valores de name y describe que coicidan con value
-	Product.findAll({WHERE:{
-		[Op.or]:[{name: value}, {describe: value}],
-		[Op.like]:'%value%'
-	}
-})
-.then((values) => {
-		res.status(OK).send({values})
-		})
+	//s27
+	server.delete('/:id',function(req,res,next){
+		var idProducto = req.params.id;
 
-});
+		Product.destroy({
+			where:
+				{
+					id: idProducto //this will be your id that you want to delete
+				}
+			}).then(function(rowDeleted){
+				// rowDeleted will return number of rows deleted
+				if(rowDeleted >= 1){
+					console.log('Deleted successfully');
+					res.status(OK).json({message:'Deleted successfully',rowDeleted});
+				}else {
+					res.status(ERROR).json({message:'Deleted failed',rowDeleted});
+				}
 
-//s24
-server.get('/:id', (req, res) =>{
- //retorna un producto por id
- const {id} = req.params;
+			});
+	})
 
-return Product.findOne({
-							 WHERE:{
-									id: id
-								}
-							})
 
- .then((products) => {
-		 if(!products){
-			 return res.status(NOT_FOUND).send(new Error('Not found!'))
-		 }
-	 res.status(OK).send({products});
- })
+	//s23
+	server.get('/search', (req, res) =>{
+		const value = req.query.query;
 
-});
+	//SELECT name, describe FROM product WHERE name OR describe LIKE '%value%'
+		if(!value || !isNaN(value)){
+			return res.sendStatus(ERROR);
+		}// Encuentra todos los valores de name y describe que coicidan con value
+		Product.findAll({WHERE:{
+			[Op.or]:[{name: value}, {describe: value}],
+			[Op.like]:'%value%'
+		}
+	})
+	.then((values) => {
+			res.status(OK).send({values})
+			})
+
+	});
+
+	//s24
+	server.get('/:id', (req, res) =>{
+	 //retorna un producto por id
+	 const {id} = req.params;
+// se hace commit para emparejar con sus cambios
+	return Product.findOne({where:{id : id}} )
+
+		// 						 include: [
+								// 	 {
+								// 		 model: Categories,
+								// 		 as : "category",
+								// 		 attributes: ...
+								// 	 },
+								//  ],
+
+
+	 .then((products) => {
+			 if(!products){
+				 return res.status(NOT_FOUND).send(new Error('Not found!'))
+			 }
+		 res.status(OK).send({products});
+	 })
+
+	});
 
 
 
