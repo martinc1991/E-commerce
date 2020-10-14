10dbb9bddaeee2d52c0d590e4968d831d7ebdf23
Camilolin <camilolindarte1992@gmail.com>
2020-10-14 13:03:51 -0500

    Arquitectura Redux version 1 Camilo

--- client/src/components/Catalogo/index.jsx
+++ client/src/components/Catalogo/index.jsx
@@ -3,7 +3,9 @@ import axios from 'axios';
 import {useState, useEffect} from 'react'
 import ProductCard from '../ProductCard/index';
 import Navegacion from '../Navegacion/Navegacion'
+import Filter from '../Filter/index';
 import {Container, Row, Col} from 'react-bootstrap';
+import s from '../../styles/catalogo.module.css';
 const url = 'localhost:3001';
 
 var enlacesUser = [
@@ -18,6 +20,58 @@ var enlacesUser = [
 const Catalogo = (props)=> {
 
     const [data, setData] = useState([]);
+    const [categories, setCategories] = useState([]);
+
+
+
+    const onSelect = (select)=> {
+		axios.get(`http://${url}/catalogo/`)
+			 .then(res => {
+				 let {data} = res.data
+				 console.log(data)
+				 return
+                
+			 })
+    };
+
+       
+    const getCategory = () => {
+        axios.get(`http://${url}/products/category`)
+            .then(res => {
+                if(res) return setCategories(res.data.result)
+                else console.log("No hay Datos")
+            })
+            .catch(err => {
+                console.log('Error')
+            })
+    };
+    
+    const handlerSelect= (e)=> {
+        const catName = e.target.value;
+        let obj = {
+            [e.target.value]: e.target.checked
+        };
+        if(obj[e.target.value] === false){
+            setData([]);
+            getProduct();
+        } else {
+        console.log(obj)
+        getCategory();
+        console.log(catName);
+        axios.get(`http://${url}/products/category/${catName}`)
+        .then(res => {
+            if(res){
+                console.log(res.data.data)
+                return setData(res.data.data)
+            }else{
+                console.log("No hay Datos")
+            }
+        })
+        .catch(err => {
+            console.log('Errod')
+        })
+        }
+	};
 
     const getProduct = () => {
         axios.get(`http://${url}/products`)
@@ -37,6 +91,7 @@ const Catalogo = (props)=> {
 
     useEffect(()=> {
         getProduct();
+        getCategory();
     }, [])
 
     console.log(props.products)
@@ -46,14 +101,15 @@ const Catalogo = (props)=> {
 
     <div>
         < Navegacion links={enlacesUser} showSearchbar={true} onSearch={props.onSearch}/>
-        <h5><a href="/products/catalogo">Mostrar todos</a></h5>
+        <h5><a href="/products/catalogo" className={s.title5}>Mostrar todos</a></h5>
         {products.length == 0  ? 
-        data.length == 0 ? <h1>No hay registros en la base de datos</h1>
+        data.length == 0 ? <h1 className={s.title2}>No hay registros en la base de datos</h1>
         :
         <Container>
-            <h1>Registros encontrados: {data.length}</h1>
+            <h1 className={s.title1}>Registros encontrados: {data.length}</h1>
+            < Filter categories={categories} handlerSelect={handlerSelect}/>
             <Row >
-           
+
             {data.map((p)=> {
                 return (
                     <Col lg="3">
@@ -72,7 +128,8 @@ const Catalogo = (props)=> {
         </Container>
         :
         <Container>
-        <h1>Registros encontrados: {products.length}</h1>
+        <h1 className={s.title1}>Registros encontrados: {products.length}</h1>
+        < Filter categories={categories} handlerSelect={handlerSelect}/>
         <Row>
         {products.map((p)=> {
             return (
